d6a0189d8f866dc84e2f38c890127ec9a765abbd
andrezlucas <andrepereiradv@gmail.com>
2025-08-24 00:42:13 -0300

    aulas

---
+++ BackendJS-softex-atividades/atividade1.js
@@ -0,0 +1,42 @@
+let readline = require ("readline-sync") ;
+
+console.log ("------------Formulário de inscrição------------");
+console.log("\n");
+
+let seunome = readline.question ("Nome completo:");
+console.log("\n");
+let cpf = readline.question ("Cpf:");
+console.log("\n");
+let tele = readline.question ("Numero de telefone/celular:");
+console.log("\n");
+let email = readline.question ("E-mail:");
+console.log("\n");
+let nasci = readline.question ("Data de nascimento:");
+console.log("\n");
+let profi = readline.question ("Profissão:");
+console.log("\n");
+let endereço = readline.question ("Endereco:");
+console.log("\n");
+let bairro = readline.question ("Bairro:");
+console.log("\n");
+let cidade = readline.question ("Cidade:");
+console.log("\n");
+
+console.log(`-----------Formulário de inscrição de ${seunome}-----------`);
+
+
+console.log ("Nome:"+seunome);
+console.log ("Cpf:"+cpf);
+console.log ("Número de telefone:"+tele);
+console.log ("E-mail:"+email);
+console.log ("Data de nascimento:"+nasci);
+console.log ("Profissão:"+profi);
+console.log ("Endereço:"+endereço);
+console.log ("Bairro:"+bairro);
+console.log ("Cidade:"+cidade);
+console.log("-----------------------------------------------------------");
+
+
+
+            
+
